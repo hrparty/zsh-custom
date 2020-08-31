0a006acd87c231b05a5fdca9deaa8d5ea8039b4f
Hlynur Tryggvason <hlynur@wuxinextcode.com>
2020-08-31 12:10:27 +0000

    Remove outdated docker clean shortcut

--- 01_base.zsh
+++ 01_base.zsh
@@ -17,8 +17,5 @@ export EDITOR="vim"
 # convenience shortcuts
 alias shellrefresh=". ~/.zshrc"
 
-# Cleans up untagged docker images
-alias dockerclean="docker ps -a | awk 'NR > 1 {print $1}' | xargs docker rm -f; docker rmi $(docker images -q -f dangling=true)"
-
 # https://github.com/superbrothers/zsh-kubectl-prompt
 RPROMPT='%{$fg[blue]%}($ZSH_KUBECTL_PROMPT)%{$reset_color%}'
